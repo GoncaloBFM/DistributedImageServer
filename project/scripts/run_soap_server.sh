587636fc5c0ac2528d403fbb3ac56f6f66a4b7cf
andrembpontes <andrembpontes@gmail.com>
2016-04-08 00:35:37 +0100

    Cache improve

--- project/scripts/run_soap_server.sh
+++ project/scripts/run_soap_server.sh
@@ -1,1 +1,6 @@
+if [[ $# -eq 1 ]]; then
+	java -classpath out/production/PictureServer/ sd.tp1.server.soap.SoapServerRun server_dir.$1 Server$1 $(( 8000 + $1 ))
+	exit
+fi
+
 java -classpath out/production/PictureServer/ sd.tp1.server.soap.SoapServerRun $@

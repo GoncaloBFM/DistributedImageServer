4ccd8d7c6fda7ab4283df3ac44f34bd8a813082c
André Pontes <andrembpontes@gmail.com>
2016-05-07 23:24:00 +0100

    ADD RestSSL server implemented

--- project/src/sd/tp1/server/rest_ssl/RestSSLServerRun.java
+++ project/src/sd/tp1/server/rest_ssl/RestSSLServerRun.java
@@ -5,16 +5,21 @@ import org.glassfish.jersey.server.ResourceConfig;
 import sd.tp1.server.HeartbeatAnnouncer;
 import sd.tp1.server.ServiceAnnouncer;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import javax.ws.rs.core.UriBuilder;
-import java.io.File;
+import java.io.*;
 import java.net.URI;
 import java.nio.file.NotDirectoryException;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 /**
  * Created by gbfm on 3/29/16.
  */
 public class RestSSLServerRun {
-    private static final String SERVICE_TO_ANNOUNCE = "42845_43178_REST";
+    private static final String SERVICE_TO_ANNOUNCE = "42845_43178_REST_SSL";
     private static final int ANNOUNCE_ON_PORT = 6968;
 
     private static final int MIN_PORT = 1024; //1024;
@@ -24,6 +29,10 @@ public class RestSSLServerRun {
     private static final String DEFAULT_ROOT = ".";
     private static final String DEFAULT_SERVICE_PATH = "PictureServer";
 
+    private static final File KEYSTORE = new File("./server.jks");
+    private static final char[] JKS_PASSWORD = "changeit".toCharArray();
+    private static final char[] KEY_PASSWORD = "changeit".toCharArray();
+
 
     public static void main(String args[]) {
         File root = new File((args.length >= 1) ? args[0] : DEFAULT_ROOT);
@@ -32,7 +41,7 @@ public class RestSSLServerRun {
 
         System.out.println(String.format("Server started at port %s, root:%s, path:%s", port, root.getAbsoluteFile(), serverPath));
 
-        URI baseUri = UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
+        URI baseUri = UriBuilder.fromUri("https://0.0.0.0/").port(port).build();
         ResourceConfig config = new ResourceConfig();
         RestSSLServer server = null;
         try {
@@ -42,6 +51,27 @@ public class RestSSLServerRun {
             System.exit(1);
         }
         config.register(server);
+
+        try{
+            SSLContext sslContext = SSLContext.getInstance("TLSv1");
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+
+            InputStream is = new FileInputStream(KEYSTORE);
+            keyStore.load(is, JKS_PASSWORD);
+
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            kmf.init(keyStore, KEY_PASSWORD);
+
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(keyStore);
+
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
         JdkHttpServerFactory.createHttpServer(baseUri, config);
 
         ServiceAnnouncer serviceAnnouncer = new HeartbeatAnnouncer(SERVICE_TO_ANNOUNCE,ANNOUNCE_ON_PORT, serverPath, port);
